82f0bf70d448ead6d8db716289355750fcb63b11
T-Sabatier <t.sabatier04@laposte.net>
2025-12-09 15:52:53 +0100

    Ajout page d'erreur

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,12 +1,10 @@
 import { NavLink } from "react-router-dom";
+import "./Error.scss"
 
 function Error() {
   return (
     <div className="Erreur">
       <h2 className="Erreur__title">404</h2>
-      <h3 className="Erreur__Subtitle">
-        Oups ! La page que vous demandez n'existe pas
-      </h3>
       <NavLink to="/">Retourner sur la page d'accueil</NavLink>
     </div>
   );
